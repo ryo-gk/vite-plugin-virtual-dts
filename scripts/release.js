068be3cda62e965af123a98d6cf319177550eabd
Ryo_gk <62658104+ryo-gk@users.noreply.github.com>
2023-01-29 12:45:12 +0900

    chore: tweak script

--- scripts/release.js
+++ scripts/release.js
@@ -1,12 +1,17 @@
 const execa = require('execa')
+const { start } = require('repl')
 const version = require('../package.json').version
 
 const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts })
 
-await run('pnpm', ['run', 'build'])
+async function start() {
+  await run('pnpm', ['run', 'build'])
 
-await run('git', ['tag', '-a', `v${version}`, '-m', `v${version}`])
+  await run('git', ['tag', '-a', `v${version}`, '-m', `v${version}`])
 
-await run('git', ['push', 'origin', `tags/v${version}`])
+  await run('git', ['push', 'origin', `tags/v${version}`])
 
-await run('npm', ['publish', './'])
+  await run('npm', ['publish', './'])
+}
+
+start()
